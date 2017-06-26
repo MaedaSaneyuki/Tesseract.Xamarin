115696c0d2e888c72c55f7e209103d4de1265108
MaedaSaneyuki <maedasaneyuki>
2017-06-26 21:26:01 +0900

    no message

---
+++ Tesseract.Forms.Test/Tesseract.Test.Droid/CameraPreviewRenderer.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace Tesseract.Test.Droid
+{
+    public class CameraPreviewRenderer : View
+    {
+
+        Paint p;
+        public Bitmap ToInterpret { private get; set; }
+        private List<Tesseract.Result> results = new List<Result>();
+        
+        public CameraPreviewRenderer(Context context, IAttributeSet attrs) :
+            base(context, attrs)
+        {
+            Initialize();
+        }
+
+        public CameraPreviewRenderer(Context context, IAttributeSet attrs, int defStyle) :
+            base(context, attrs, defStyle)
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            SetWillNotDraw(false);
+
+            // 塗りつぶしの設定
+            p = new Paint();
+            p.AntiAlias = (true);
+            p.Dither=(true);
+            p.Color=(Color.Black);
+            p.Alpha=(128);
+            p.SetStyle(Paint.Style.FillAndStroke);
+        
+        }
+
+        public void ClearResults()
+        {
+            results.Clear();
+        }
+
+        public void ImplementResult(Tesseract.Result result)
+        {
+            if (result == null) return;
+            var r = new Tesseract.Result();
+            r.Box = result.Box;
+            r.Confidence = result.Confidence;
+            r.Text = result.Text;
+            results.Add(r);
+        }
+
+
+        private int cnt = 0;
+        public System.Threading.SemaphoreSlim syncObj = new System.Threading.SemaphoreSlim(1,1);
+
+        public override void Draw(Canvas cv)
+        {
+            base.Draw(cv);
+         //   syncObj.Wait();
+            try
+            {
+                p.Color = Color.White;
+                p.Alpha = 0;
+                cv.DrawRect(0, 0, Width, Height, p);
+                p.Color = Color.Black;
+                p.Alpha = 128;
+                cv.DrawRect(0, Height / 3, Width, Height, p);
+                var src = new RectF(0, 0, Width, 1280 / 3);
+                var centerClip = new Size(
+                      (int)(src.Width() * 0.30)
+                    , (int)(src.Height() * 0.48));
+
+                var left = (src.Width() - centerClip.Width) / 2;
+                var top = (1040 /3 - centerClip.Height) / 2;
+                var bottom = top + centerClip.Height;
+                var rigtht = left + centerClip.Width;
+                p.Color = Android.Graphics.Color.Wheat;
+                p.SetStyle(Paint.Style.Stroke);
+                p.Alpha = 255;
+                p.StrokeWidth = 6;
+                cv.DrawRect(left, top, rigtht, bottom, p);
+                p.StrokeWidth = 1;
+
+                p.SetStyle(Paint.Style.FillAndStroke);
+                if (results.FirstOrDefault() != null && results.FirstOrDefault().Confidence > 0)
+                {
+                    try
+                    {
+
+                        var r = results.FirstOrDefault();
+                        Android.Util.Log.Debug("overlay", "Word: \"{0}\", confidence: {1}", r.Text, r.Confidence);
+
+                        p.Alpha = 255;
+                        p.Color = Color.Blue;
+                        p.TextSize = 130;
+
+                        cv.DrawText(r.Text, r.Box.X, r.Box.Y, p);
+                    }
+                    catch (Exception ex)
+                    {
+                        Android.Util.Log.Debug("overlay", ex.ToString());
+                    }
+                }
+
+                if(ToInterpret!=null&&ToInterpret.Width>0)
+                    
+                cv.DrawBitmap(ToInterpret, (Width- ToInterpret.Width)/2, (float)( Height * 0.4), p);
+
+
+                p.Color = Color.Yellow;
+                p.TextSize = 30;
+                p.Alpha = 255;
+                cv.DrawText((cnt++).ToString(), 0,(float)(Height * 0.9), p);
+            }
+            finally
+            {
+               // syncObj.Release();
+            }
+            
+        }
+
+    }
+}
---
+++ Tesseract.Forms.Test/Tesseract.Test.Droid/Converter.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Graphics;
+using System.IO;
+using System.Drawing;
+
+
+namespace Tesseract.Test.Droid
+{
+    public class Converter
+    {
+        public Bitmap ToTesseract { get; private set; }
+        public Bitmap ToInterpret { get; private set; }
+
+        private static int dr = 45;
+        private static int dbX = -400;
+        private static int dbY = -200;
+        public static int YLevelBorder = 215;
+
+
+        public virtual RectangleF sorceClip(Size cameraParameter)
+        {
+          //  return  new RectangleF(0, 0, cameraParameter.Width, cameraParameter.Height / 3);
+            return  new RectangleF(0, 0, cameraParameter.Height, cameraParameter.Width / 3);
+        }
+
+        public virtual Rect sorceClipRect(Size cameraParameter)
+        {
+            var src = sorceClip(cameraParameter);
+            return new Rect((int)src.Left, (int)src.Top, (int)src.Right, (int)src.Bottom);
+        }
+
+
+
+        public virtual byte[] ConvertYuvToJpeg(byte[] yuvData,ImageFormatType cameraFotmat, Size previewSize , Size centerClip  )
+        {
+            for(var ite =0;ite< previewSize.Width * previewSize.Height; ite +=2)
+            {
+                if(yuvData[ite]<YLevelBorder)
+                {
+                    yuvData[ite] = 255;
+                    yuvData[ite + 1] = 255;
+                  //  yuvData[ite + 2] = 0;
+                }
+                else
+                {
+                    yuvData[ite] = 0;
+                    yuvData[ite + 1] = 0;
+                    //    yuvData[ite + 2] = 0;
+                }
+            }
+            for (var ite = previewSize.Width * previewSize.Height; ite < yuvData.Length; ite++)
+            {
+                yuvData[ite] = 130;
+            }
+
+
+
+            var yuv = new YuvImage(yuvData, cameraFotmat, previewSize.Width, previewSize.Height, null);
+            byte[] jpegData;
+            using (var ms = new MemoryStream())
+            {
+                var quality = 100;
+                // yuv.CompressToJpeg(sorceClipRect(cameraParameter), quality, ms);
+                 yuv.CompressToJpeg(new Rect(0,0,previewSize.Width,previewSize.Height), quality, ms);
+                jpegData = ms.ToArray();
+            }
+
+            try
+            {
+
+                var src = sorceClip(previewSize);
+                BitmapFactory.Options bitmapFatoryOptions = new BitmapFactory.Options();
+                bitmapFatoryOptions.InPreferredConfig = Bitmap.Config.Rgb565;
+                bitmapFatoryOptions.InMutable = true;
+
+                //１．Bitmapの生成時にオプションを与えてmutableなオブジェクトを生成する。
+                //http://dev.classmethod.jp/smartphone/android/android-immutable-bitmap-mutable/
+                var srcbmp = BitmapFactory.DecodeByteArray(jpegData, 0, jpegData.Length, bitmapFatoryOptions);
+                var m = new Matrix();
+                //   m.PostTranslate(-1 * previewSize.Width, -1 * previewSize.Height / 3);
+                   m.PostRotate(90);
+             //   m.PostRotate(dr);
+                 m.PostTranslate(previewSize.Height, 0);
+               // m.PostTranslate(dbX, dbY);
+
+                //  var bmp = Bitmap.CreateBitmap(srcbmp, 0, 0, (int)src.Width, (int)src.Height, m, true);
+                //  var bmp = Bitmap.CreateBitmap((int)src.Width, (int)src.Height, srcbmp.GetConfig());                       
+                var bmp = Bitmap.CreateBitmap((int)src.Width, (int)src.Height, srcbmp.GetConfig());
+                var left = (src.Width - centerClip.Width) / 2;
+                var top = (src.Height - centerClip.Height) / 2;
+                var bottom = top + centerClip.Height;
+                var rigtht = left + centerClip.Width;
+                using (var cv = new Canvas(bmp))
+                {   
+                    var p = new Paint();
+                    p.Color = Android.Graphics.Color.White;
+                    p.SetStyle(Paint.Style.Fill);
+                    cv.DrawBitmap(srcbmp, m, p);
+
+                    cv.DrawRect(0, 0, left, src.Bottom, p);
+                    cv.DrawRect(0, 0, src.Width, top, p);
+                    cv.DrawRect(0, bottom, src.Width, src.Bottom, p);
+                    cv.DrawRect(rigtht, 0, src.Right, src.Bottom, p);
+                }
+                //ToTesseract = bmp.Copy(bmp.GetConfig(), true);
+
+                var interpret = Bitmap.CreateBitmap(bmp, (int)left, (int)top, centerClip.Width, centerClip.Height);
+                //ToInterpret = bmp.Copy(bmp.GetConfig(), true);
+                ToInterpret = interpret;
+                using (var ms = new MemoryStream())
+                {
+                    var quality = 100;
+                    bmp.Compress(Bitmap.CompressFormat.Jpeg, quality, ms);
+                    jpegData = ms.ToArray();
+                }
+            }
+            catch(Exception ex)
+            {
+                Android.Util.Log.Debug("convert", ex.ToString());
+            }
+            return jpegData;
+        }
+    }
+
+    public class LedSegConverter : Converter
+    {
+        
+        
+
+    }
+
+
+}
